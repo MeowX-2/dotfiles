973603596dc5d6099df17dcf51b3333c79978c8a
MeowX-2 <alamarifa9@gmail.com>
2024-01-19 00:32:59 +0600

    neorg

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -4,6 +4,8 @@ vim.cmd("set rnu")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
+vim.cmd("set breakindent")
+vim.cmd("set linebreak")
 --vim.g.mapleader =  
 
 
