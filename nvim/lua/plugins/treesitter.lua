189694d7490204d5dc6e8a47c653e8d7e9aa8b37
MeowX-2 <alamarifa9@gmail.com>
2024-01-19 01:50:24 +0600

    .

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
      config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
-        ensure_installed = {"lua", "javascript", "latex", "html","css","java","matlab", "perl","python", "c", "latex", "cpp", "vim","vimdoc"},
+        ensure_installed = {"yaml","toml","xml","php","pascal","meson", "norg","lua", "javascript", "latex", "html","css","java","matlab", "perl","python", "c", "latex", "cpp", "vim","vimdoc", "fish", "fsh", "javascript", "rust", "ruby", "nix", "make", "c_sharp", "bash", "bibtex"},
         highlight = { enable = true },
         indent = { enable = true },  
       })
