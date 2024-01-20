ae21e72e584558138dc902c45e4667de09bc5566
MeowX-2 <alamarifa9@gmail.com>
2024-01-20 23:31:29 +0600

    neorg mode tweaks

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -4,7 +4,7 @@ return {
      config = function()
       local config = require("nvim-treesitter.configs")
       config.setup({
-        ensure_installed = {"yaml","toml","xml","php","pascal","meson", "norg","lua", "javascript", "latex", "html","css","java","matlab", "perl","python", "c", "latex", "cpp", "vim","vimdoc", "fish", "fsh", "javascript", "rust", "ruby", "nix", "make", "c_sharp", "bash", "bibtex"},
+        ensure_installed = {"yaml","toml","xml","php","pascal","meson", "norg","lua", "javascript", "latex", "html","css","java","matlab", "perl","python", "c", "latex",  "vim","vimdoc", "fish", "fsh", "javascript", "rust", "ruby", "nix", "make", "c_sharp", "bash", "bibtex"},
         highlight = { enable = true },
         indent = { enable = true },  
       })
