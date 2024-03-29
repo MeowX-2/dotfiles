e930363bcf08d8972b9abc6f5a8b64ecdf08dc82
MeowX-2 <alamarifa9@gmail.com>
2024-03-29 16:08:55 +0600

    "snippets"

---
+++ nvim/snippets/tex/math.lua
@@ -0,0 +1,27 @@
+local in_mathzone = function()
+  -- The `in_mathzone` function requires the VimTeX plugin
+  return vim.fn['vimtex#syntax#in_mathzone']() == 1
+end
+
+return {
+    s("ff", fmta(
+        [[
+\frac{<>}{<>}
+]],{i(1), i(2)}),{condition = in_mathzone, show_condition = in_mathzone}),
+
+    s("df", fmta(
+        [[
+\dv{<>}{<>}
+]],{i(1), i(2)}), {condition = in_mathzone, show_condition = in_mathzone}),
+
+    s("int", fmta(
+        [[
+\int_{<>}^{<>}
+]],{i(1), i(2)}),{condition = in_mathzone, show_condition = in_mathzone}),
+
+    s("sum", fmta(
+        [[
+\sum_{<>}^{<>}
+]],{i(1), i(2)}), {condition = in_mathzone, show_condition = in_mathzone}),
+
+}
